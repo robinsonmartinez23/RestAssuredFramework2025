2edd8de1e77c1ac3c0937d49df1d076a290229c9
robinsonmartinez23 <robinsonmartinez23@hotmail.com>
2025-06-03 21:24:26 -0400

    BaseTaste Uncomment @Listener line 11

--- src/test/java/com/qa/api/base/BaseTest.java
+++ src/test/java/com/qa/api/base/BaseTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 
-//@Listeners(ChainTestListener.class)
+@Listeners(ChainTestListener.class)
 public class BaseTest {
 
     protected RestClient restClient;
